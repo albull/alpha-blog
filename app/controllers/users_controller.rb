0ab1de97fe533c7e7374aa8fa074f51d835155dc
Andrew Bull <albull@umich.edu>
2017-06-05 03:28:03 +0000

    sign in on sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -57,6 +58,7 @@ class UsersController < ApplicationController
     if !logged_in? && current_user != @user
       flash[:danger] = "You can only edit your own account"
       redirect_to root_path
+    end
   end
   
 end
